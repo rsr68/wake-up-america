d0fe0be6a28b591317b382e42d922bdb4d3bebf3
Michelle Bergeron <mrb113@case.edu>
2015-02-28 02:13:45 -0800

    simplify content script to not use the dictionary for now. Leaving the dictionary file in there though

--- content_script.js
+++ content_script.js
@@ -1,63 +1,42 @@
-// MB: Much of this is modified from https://github.com/snipe/downworthy
-(function() {
-	var _self = this;
-	var _dictionary;
+walk(document.body);
 
-	function getDictionary(callback) {
-		chrome.extension.sendRequest({id: "getDictionary"}, function(response) {
-			_dictionary = response; // Store the dictionary for later use.
-			callback.apply(_self, arguments);
-		});
+function walk(node) 
+{
+	// Function from StackOverflow:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
 	}
+}
 
-	function handleText(textNode) {
-		var replacements = _dictionary.replacements;
-		var v = textNode.nodeValue;
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+	
+	// Really need a dictionary
+	v = v.replace(/\bMichelle Obama\b/g, "MOOCHelle Obimbo");
+	v = v.replace(/\bAffordable Care Act\b/g, "Obongocare");
+	v = v.replace(/\bObamacare\b/g, "Owebamacare");
+	v = v.replace(/\bBarack\b/g, "Hussein");
+	v = v.replace(/\bObama\b/g, "Obummer");
+	textNode.nodeValue = v;
+}
 
-		var regex, original;
-
-		for(original in replacements) {
-			regex = new RegExp('\\b' + original + '\\b', 'g');
-			v = v.replace(regex, replacements[original]);
-		}
-			
-		textNode.nodeValue = v;
-	}
-
-	function walk(node) {
-
-		// I stole this function from here: - ZW
-		// And I stole it from ZW - AG
-		// http://is.gd/mwZp7E
-		
-		var child, next;
-
-		switch(node.nodeType) {
-			case 1:  // Element
-			case 9:  // Document
-			case 11: // Document fragment
-				child = node.firstChild;
-				while(child) {
-					next = child.nextSibling;
-					walk(child);
-					child = next;
-				}
-				break;
-			case 3: // Text node
-				handleText(node);
-				break;
-		}
-	}
-
-	chrome.extension.sendRequest({id: 'isPaused?'}, function(response) {
-		var isPaused = response.value;
-		console.log('isPaused is ' + isPaused);
-
-		if(!isPaused) {
-			getDictionary(function() {
-				walk(document.body);
-			});
-		}
-	});
-
-})();
