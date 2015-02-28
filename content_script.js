82783395c26991c9f334dbaec67c84f3173dea01
Michelle Bergeron <mrb113@case.edu>
2015-02-28 02:58:48 -0800

    Added more derogatory terms to replace nice ones with

--- content_script.js
+++ content_script.js
@@ -1,3 +1,4 @@
+// Inspired by and modified from https://github.com/snipe/downworthy
 walk(document.body);
 
 function walk(node) 
@@ -32,11 +33,20 @@ function handleText(textNode)
 	var v = textNode.nodeValue;
 	
 	// Really need a dictionary
-	v = v.replace(/\bMichelle Obama\b/g, "MOOCHelle Obimbo");
-	v = v.replace(/\bAffordable Care Act\b/g, "Obongocare");
-	v = v.replace(/\bObamacare\b/g, "Owebamacare");
-	v = v.replace(/\bBarack\b/g, "Hussein");
-	v = v.replace(/\bObama\b/g, "Obummer");
+	v = v.replace(/\bMichelle Obama\b/ig, "MOOCHelle Obimbo");
+	v = v.replace(/\bAffordable Care Act\b/ig, "Obongocare");
+	v = v.replace(/\bObamacare\b/ig, "Owebamacare");
+	v = v.replace(/\bBarack\b/ig, "Hussein");
+	v = v.replace(/\bObama\b/ig, "Obummer");
+	v = v.replace(/\bMuslim\b/ig, "mooSLIME");
+	v = v.replace(/\bMuslims\b/ig, "Muzzies");
+	v = v.replace(/\bObamacare\b/ig, "Owebamacare");
+	v = v.replace(/\bBlack\b/ig, "Thug");
+	v = v.replace(/\bObamas\b/ig, "Obozos");
+	v = v.replace(/\bISIS\b/ig, "Obi-Wan Nairobi's Muslim Army");
+	
+	v = v.replace(/\."/ig, ". WAKE UP AMERICA\" ");
+	v = v.replace(/\?/ig, "? This country is going to Hell!!!!!\" ");
 	textNode.nodeValue = v;
 }
 
